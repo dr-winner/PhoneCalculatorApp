8f19656dfd6472d694ab20a585d5036387170f6b
dr-winner <duvorrichardwinner@gmail.com>
2024-02-12 19:06:17 +0000

    complete 2

--- iOSCalc/main.js
+++ iOSCalc/main.js
@@ -18,3 +18,32 @@ function calculate(){
         dspl.value = "Invalid";
     }
 } 
+
+let currentValue = '';
+let operator = null; 
+
+function updateDisplay(number) {
+  currentValue += number;
+  document.getElementById('display').value = currentValue;
+}
+
+const clearButton = document.getElementById("clear");
+
+clearButton.addEventListener('click', () => {
+  cleardspl(); 
+});
+
+const allButtons = document.querySelectorAll('button');
+
+allButtons.forEach(button => {
+  button.addEventListener('click', handleButtonClick); 
+});
+
+function handleButtonClick(event) {
+
+  const value = event.target.textContent;
+  
+  if (isNumber(value)) {
+    appendTodspl(value);
+  } 
+}
